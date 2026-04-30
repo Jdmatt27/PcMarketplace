d75c95f7441526904bb5f59830f41aa4bd34aab3
Jdmatt27 <jdavidmatt27@gmail.com>
2026-04-30 18:24:18 +0200

    Refactor Java package from com.juanda to com.pcmarketplace

--- src/main/java/com/pcmarketplace/Main.java
+++ src/main/java/com/pcmarketplace/Main.java
@@ -1,4 +1,4 @@
-package com.juanda;
+package com.pcmarketplace;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
