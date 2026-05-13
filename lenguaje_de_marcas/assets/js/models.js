d0dc35a2b5c35fab0a9c32ef719a9337a68b9e0d
Javier <javierclementejr@gmail.com>
2026-05-13 10:00:00 +0200

    añado los modelos Cliente, Producto y Usuario

---
+++ lenguaje_de_marcas/assets/js/models.js
@@ -0,0 +1,30 @@
+class Cliente {
+    constructor(id, nombre, email, telefono, direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+}
+
+
+class Producto {
+    constructor(id, nombre, categoria, precio, stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+
+
+class Usuario {
+    constructor(id, nombre, email, rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.rol = rol;
+    }
+}
