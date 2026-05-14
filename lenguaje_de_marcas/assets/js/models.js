049d1c19f4c30f48b27865e5b96b25f4ae359ba5
Javier <javierclementejr@gmail.com>
2026-05-14 11:30:00 +0200

    termino los modelos con Venta y DetalleVenta

--- lenguaje_de_marcas/assets/js/models.js
+++ lenguaje_de_marcas/assets/js/models.js
@@ -28,3 +28,26 @@ class Usuario {
         this.rol = rol;
     }
 }
+
+
+class Venta {
+    constructor(id, clienteId, usuarioId, fecha, estado, total) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.usuarioId = usuarioId;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.total = total;
+    }
+}
+
+
+class DetalleVenta {
+    constructor(id, ventaId, productoId, cantidad, precioUnitario) {
+        this.id = id;
+        this.ventaId = ventaId;
+        this.productoId = productoId;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+}
