cdf15876d30d899730375687d2144c8f3c575ca4
JavierClemente <javierclementejr@gmail.com>
2026-05-25 02:06:13 +0200

    Cambiamos la contraseña de MySQL en DataBaseConnection

--- src/main/java/com/pcmarketplace/util/DataBaseConection.java
+++ src/main/java/com/pcmarketplace/util/DataBaseConection.java
@@ -8,7 +8,7 @@ public class DataBaseConection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/crm";
     private static final String USER = "root";
-    private static final String PASSWORD = "123456789";
+    private static final String PASSWORD = "la_contraseña_MySQL";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
